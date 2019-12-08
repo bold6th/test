35f8f11152b372980b8621bb99a7a8d0955d5d5a
鈴木大悟 <suzukidaigo@192.168.1.5>
2019-12-08 23:16:42 +0900

    test確認用プッシュ

---
+++ java_silver/src/java_silver_chpter5/Book.java
@@ -0,0 +1,12 @@
+package java_silver_chpter5;
+
+public class Book {
+private String isbn;
+
+public void setIsbn(String isbn) {
+	this.isbn = isbn;
+}
+protected void printInfo() {
+	System.out.println(isbn);
+}
+}
