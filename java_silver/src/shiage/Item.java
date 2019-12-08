8df11d11dd2125e53901aaa5ff63159db405b8ee
鈴木大悟 <suzukidaigo@192.168.1.5>
2019-12-08 23:55:02 +0900

    おっぱい

---
+++ java_silver/src/shiage/Item.java
@@ -0,0 +1,13 @@
+package shiage;
+
+public class Item {
+	int id;
+	String name;
+	public Item(int id , String name) {
+		super();
+		this.id = id;
+	    this.name = name;
+
+	}
+
+}
---
+++ java_silver/src/test/Boolean.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Boolean {
+
+	public static void main(String[] args) {
+		int val = 7;
+		boolean flg = true;
+		if(flg == true) {
+			do {
+				System.out.println(val);
+			}while(val > 10);
+		}
+
+	}
+
+}
